323af8ee55b9b5d0cab60df6c423af5ce3c822a9
unknown <pravinkumar12@gmail.com>
2024-10-20 21:38:00 +0100

    Adding Listernes Concept

---
+++ PageObjectModelFramework/src/test/java/Regression/HomeTest.java
@@ -0,0 +1,10 @@
+package Regression;
+
+public class HomeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- PageObjectModelFramework/src/test/java/com/listners/ListeningTest.java
+++ PageObjectModelFramework/src/test/java/com/listners/ListeningTest.java
@@ -4,20 +4,30 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.Status;
+
+import utility.ExtentManager;
+import utility.ExtentTestManager;
+
 public class ListeningTest implements ITestListener {
 	
 	
 	public  void onTestStart(ITestResult result) {
 		
 		System.out.println("Running Test Method"+ result.getMethod().getMethodName());
+		
+		ExtentTestManager.startTest( result.getMethod().getMethodName());
+		
 	    
 	  }
 
 	  
 	  public void onTestSuccess(ITestResult result) {
 	    // not implemented
-		  
-		  System.out.println("Test Executed Pass "+result.getMethod().getMethodName());
+		 
+		 System.out.println("Test Executed Pass "+result.getMethod().getMethodName());
+	 
+	  ExtentTestManager.getTest().log(Status.PASS, "Passed");
 	  }
 
 	  
@@ -25,7 +35,7 @@ public class ListeningTest implements ITestListener {
 	    // not implemented
 		  
 		  System.out.println("Test Executed Failed "+result.getMethod().getMethodName());
-
+		  ExtentTestManager.getTest().log(Status.FAIL, "Fail");
 	  }
 
 	  
@@ -34,6 +44,7 @@ public class ListeningTest implements ITestListener {
 		  
 		  System.out.println("Test Skipped "+result.getMethod().getMethodName());
 
+		  ExtentTestManager.getTest().log(Status.SKIP, "Skipped");
 	  }
 
 	 
@@ -52,12 +63,16 @@ public class ListeningTest implements ITestListener {
 	    // not implemented
 		  
 		  System.out.println("Test Suite"+ context.getName()+"Started");
+		 // ExtentTestManager.startTest(null)
 	  }
 
 	  
 	  public  void onFinish(ITestContext context) {
 	    // not implemented
 		  System.out.println("Test Finish Suite"+context.getName()+"Finish");
+		  
+		  ExtentTestManager.endTest();
+		  ExtentManager.getInstance().flush();
 	  }
 	
 	
